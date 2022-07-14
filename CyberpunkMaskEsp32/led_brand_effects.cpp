744b2db2ec307705f45ead2d8dbe6c0cb527ed6c
Sherard Lee <sherardchina@gmail.com>
2022-07-14 17:33:04 +0800

    初始化led灯带效果

--- CyberpunkMaskEsp32/led_brand_effects.cpp
+++ CyberpunkMaskEsp32/led_brand_effects.cpp
@@ -7,16 +7,37 @@
 #endif
 #include "led_brand_effects.h"
 
+int centerNum = 0;
+pixelColor_t lastColor;
+long lastFlashTime = 0L;
+
 LedBrandEffects::LedBrandEffects(strand_t * pStrand) {
-  strand_t * strands [] = { StrandPtr };
+  StrandPtr = pStrand;
+  lastColor = pixelFromRGBW(0x3F, 0x3F, 0x3F, 0x3F);
 }
 
 void LedBrandEffects::renderLed() {
-
+  long nowTime = millis();
+  if (nowTime - lastFlashTime >= 100) {
+    if (centerNum == StrandPtr->numPixels) {
+      centerNum = 0;
+      for (uint16_t i = centerNum; i < StrandPtr->numPixels; i++) {
+        StrandPtr->pixels[i] = lastColor;
+      }
+    } else {
+      for (uint16_t i = centerNum; i < StrandPtr->numPixels; i++) {
+        StrandPtr->pixels[i] = pixelFromRGBW(0x3F, 0x3F, 0x3F, 0xFF - 4 * (i - centerNum));
+      }
+      centerNum++;
+    }
+    strand_t * strands [] = { StrandPtr };
+    digitalLeds_drawPixels(strands, 1);
+  }
 }
 /**
-   设置副屏内容
+   设置效果序号
 */
 void LedBrandEffects::setEffNum(int effNum) {
   this->effNum = effNum;
+  centerNum = 0;
 }
